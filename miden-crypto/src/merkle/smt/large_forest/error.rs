24dc99a3d329b5874f0d604691d3e40b627918c0
Ara Adkins <me@ara.io>
2025-11-27 16:07:10 +0100

    Implement the history overlay mechanism

---
+++ miden-crypto/src/merkle/smt/large_forest/error.rs
@@ -0,0 +1,33 @@
+//! This module contains the error types and helpers for working with errors from the large SMT
+//! forest.
+
+use thiserror::Error;
+
+/// The type of errors returned by operations on the large SMT forest.
+#[derive(Debug, Error)]
+pub enum LargeSmtForestError {
+    #[error(transparent)]
+    HistoryError(#[from] history::HistoryError),
+}
+
+/// The result type for use within the large SMT forest portion of the library.
+pub type Result<T> = std::result::Result<T, LargeSmtForestError>;
+
+pub mod history {
+    use thiserror::Error;
+
+    use crate::Word;
+
+    /// The type of errors returned by the history subsystem of the large SMT forest.
+    #[derive(Debug, Error, PartialEq)]
+    pub enum HistoryError {
+        #[error("The root {0} had no corresponding history version")]
+        NoSuchVersion(Word),
+
+        #[error("The history contains no deltas")]
+        NothingToRemove,
+    }
+
+    /// The result type for use within the history subsystem of the large SMT forest.
+    pub type Result<T> = std::result::Result<T, HistoryError>;
+}
